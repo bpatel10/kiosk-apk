d3ecd7be3b7e90cdbee1dbeed5634860073440da
Binita Patel <binita.patel@wakefern.com>
2020-08-21 16:06:42 -0400

    fix: send message on click

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -16,103 +16,91 @@
  * specific language governing permissions and limitations
  * under the License.
 //  */
+let iframe = document.createElement("iframe");
 
-// function goHome() {	
-// 	var http = new XMLHttpRequest();
-
-// 	http.open("GET","index.html",false); // REPLACE INDEX.JSP WITH NEW URL
-// 	http.send();
-
-// 	if(http.status == 200) {
-//     console.log('were going home ')
-//     let meta = document.querySelector('meta[name="refresh"]')
-//     meta.parentNode.removeChild(meta);
-// 	} else {
-//     console.log("The server is currently down at the moment, retrying in 5 seconds.")
-//     location.reload;
-// 		setTimeout(goHome, 5000);
-// 	}
-// }
-
+iframe.setAttribute(
+  "src",
+  "http://devsrv86a/pricecheck/index.html?storeid=511&brand=shop-rite&mwgStoreId=070C334"
+  //"http://localhost:9080/pricecheck/index.html?storeid=433&brand=shop-rite&mwgStoreId=3D65777"
+);
 function doesConnectionExist() {
   var xhr = new XMLHttpRequest();
   var file = "http://kiosk.wakefern.com/kioskandroid";
   var randomNum = Math.round(Math.random() * 10000);
-
-  xhr.open('HEAD', file + "?rand=" + randomNum, true);
+  xhr.open("HEAD", file + "?rand=" + randomNum, true);
   xhr.send();
-   
   xhr.addEventListener("readystatechange", processRequest, false);
-
   function processRequest(e) {
     if (xhr.readyState == 4) {
       if (xhr.status >= 200 && xhr.status < 304) {
         alert("connection exists!");
-        let iframe  = document.createElement('iframe');
-// iframe.setAttribute('src','http://devsrv86a/pricecheck/index.html?storeid=511&brand=shop-rite&mwgStoreId=070C334')
-iframe.setAttribute('src','http://devsrv86a.wakefern.com/pricechecker/#/')
-            document.body.appendChild(iframe);
-
+        document.body.appendChild(iframe);
+        document
+          .getElementById("clickme")
+          .addEventListener("click", function () {
+            sendMessage();
+          });
       } else {
-        alert("connection doesn't exist!");
-        setTimeout(doesConnectionExist, 5000)
+        //alert("connection doesn't exist!");
+        //setTimeout(doesConnectionExist, 5000);
+        document.body.appendChild(iframe);
       }
     }
   }
+  function sendMessage() {
+    console.log("sending data");
+    console.log(iframe);
+    iframe.contentWindow.postMessage({ scanData: "000024536789" }, "*");
+  }
 }
-
-
-
 var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+  // Application Constructor
+  initialize: function () {
+    document.addEventListener(
+      "deviceready",
+      this.onDeviceReady.bind(this),
+      false
+    );
+  },
+  // deviceready Event Handler
+  //
+  // Bind any cordova events here. Common events are:
+  // 'pause', 'resume', etc.
+  onDeviceReady: function () {
+    this.receivedEvent("deviceready");
+    doesConnectionExist();
+  },
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-        // inAppBrowserRef = cordova.InAppBrowser.open('https://deliorder.shoprite.com', '_blank', 'location=no,clearsessioncache=yes');
-        // let script = 'alert("fuck you")'
-        // inAppBrowserRef.addEventListener('loadstop', function() {
-        //      inAppBrowserRef.executeScript({code: script});
-        //  });
-        doesConnectionExist();
-
-      },
-
-    // Update DOM on a Received Event
-
-    receivedEvent: function(id) {
-        // var parentElement = document.getElementById(id);
-        // var listeningElement = parentElement.querySelector('.listening');
-        // var receivedElement = parentElement.querySelector('.received');
-        // listeningElement.setAttribute('style', 'display:none;');
-        // receivedElement.setAttribute('style', 'display:block;');
-    }
+  // Update DOM on a Received Event
+  receivedEvent: function (id) {},
 };
 
 app.initialize();
-setTimeout(function(){
-    // alert('plugin loaded', HedcPlugin)
-    console.log('plugin loaded', HedcPlugin)
-
-      var success = function(message) {
-        alert("javascriptside: " + message);
-        console.log("javascriptside: " + message);
-
-        console.log('iframe.contentWindow.postMessage(message, )',iframe.contentWindow.postMessage('4011', 'http://devsrv86a/pricecheck/index.hml?storeid=511&brand=shop-rite&mwgStoreId=070C334'))
-        iframe.contentWindow.postMessage(message, 'http://devsrv86a/pricecheck/index.html?storeid=511&brand=shop-rite&mwgStoreId=070C334');
-      }
-      
-      var failure = function(error) {
-        // alert("Callback sent failed");
-        console.log("Callback sent failed",error);
-      }
-      
-      HedcPlugin.sendWrappedCommand('defalt', success, failure)
-    },5000)
-
+setTimeout(function () {
+  var success = function (message) {
+    alert("javascriptside: " + message);
+    console.log("javascriptside: " + message);
+    console.log(
+      "iframe.contentWindow.postMessage(message, )",
+      iframe.contentWindow.postMessage(
+        "4011",
+        "http://devsrv86a/pricecheck/index.hml?storeid=511&brand=shop-rite&mwgStoreId=070C334"
+      )
+    );
+    alert(
+      "iframe.contentWindow.postMessage(message, )",
+      iframe.contentWindow.postMessage(
+        "4011",
+        "http://devsrv86a/pricecheck/index.hml?storeid=511&brand=shop-rite&mwgStoreId=070C334"
+      )
+    );
+    iframe.contentWindow.postMessage(
+      message,
+      "http://devsrv86a/pricecheck/index.html?storeid=511&brand=shop-rite&mwgStoreId=070C334"
+    );
+  };
+  var failure = function (error) {
+    console.log("Callback sent failed", error);
+  };
+  HedcPlugin.sendWrappedCommand("defalt", success, failure);
+}, 5000);
